2244eb0426031dee7202902133f197ef23daef98
Iasmin Danyelly <102995756+IasminDanyelly@users.noreply.github.com>
2022-10-25 23:10:55 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+# Intera-es
+Esse projeto é um teste de javascript,html e css,criado por mim e algumas ideias de canais no youtube. *Eu não copiei e colei*
+function displaynum(n1){
+    calculadora.text1.value = calculadora.text1.value + n1;
+}
